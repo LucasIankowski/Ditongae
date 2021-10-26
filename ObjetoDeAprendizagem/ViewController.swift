317a3978f4d07fce8f485468fddb68742f197193
Lucas Iankowski <lucasicsilva@hotmail.com>
2021-10-26 20:36:49 -0300

    Initial Commit

---
+++ ObjetoDeAprendizagem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObjetoDeAprendizagem
+//
+//  Created by Lucas Iankowski Corrêa da Silva on 26/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
